5adce71fb94d076a74de28f22f80f809bce9a2f3
Dennis <teyushen@gmail.com>
2015-04-17 00:10:56 +0800

    giverDao-unfinish

--- src/main/java/tw/org/iiiedu/thegivers/dao/GiverDao.java
+++ src/main/java/tw/org/iiiedu/thegivers/dao/GiverDao.java
@@ -4,9 +4,9 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -19,53 +19,122 @@ public class GiverDao {
 	@Autowired
 	SessionFactory sessionFactory;
 
-	//查詢帳號
+	private Session session;
+
+	public Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
+	// 查詢帳號
 	public GiverModel getByAccount(String account) {
 
 		GiverModel result = null;
 
-		Session session = sessionFactory.getCurrentSession();
-
 		try {
-			Transaction tx = session.beginTransaction();
-			Criteria criteria = session.createCriteria(GiverModel.class);
+			Criteria criteria = getSession().createCriteria(GiverModel.class);
 			criteria.add(Restrictions.eq("account", account).ignoreCase());
 			Iterator i = criteria.list().iterator();
-			
-//			Iterator giverModels = session.createCriteria(GiverModel.class)
-//					.add(Restrictions.eq("account", account).ignoreCase())
-//					.list().iterator();
 
 			if (i.hasNext()) {
 				result = (GiverModel) i.next();
 			}
-			tx.commit();
 
-		} catch (Exception e) {
+		} catch (HibernateException e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
-	//註冊帳號
-	public GiverModel insert(GiverModel bean){
-		 Session session = sessionFactory.getCurrentSession();
 
-		 Transaction tx = session.beginTransaction();
-		 Criteria criteria = session.createCriteria(GiverModel.class);
-		 criteria.add(Restrictions.eq("account", bean.getAccount()).ignoreCase());
-		 List<GiverModel> result = criteria.list(); 
-		 
-		 if(result == null){
-			 session.save(bean);
-			 tx.commit();
-			 return bean;
-		 }
-		 
-		 
-		 
-		 return null;
-		
+	// 註冊帳號
+	public GiverModel insert(GiverModel bean) {
+
+		Criteria criteria = getSession().createCriteria(GiverModel.class);
+		criteria.add(Restrictions.eq("account", bean.getAccount()).ignoreCase());
+		List<GiverModel> result = criteria.list();
+
+		try {
+			getSession().save(bean);
+			return bean;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			System.out.println("insert fail");
+		}
+
+		return bean;
 	}
 
+	// 取得ID
+	public GiverModel getById(int id) {
+
+		try {
+			GiverModel bean = (GiverModel) getSession().get(GiverModel.class,
+					id);
+			return bean;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			System.out.println("can not find this id");
+		}
+
+		return null;
+	}
+	
+	//關閉帳號 (用戶端)
+	public boolean hide(String account){
+		
+		Criteria criteria = getSession().createCriteria(GiverModel.class);
+		Iterator iterator = criteria
+				.add(Restrictions.eq("account", account).ignoreCase()).list().iterator();
+		try {
+			if (iterator.hasNext()) {
+				GiverModel bean = (GiverModel) iterator.next();
+				bean.setValid(false);
+				return true;
+			}
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	//getAll
+	public List<GiverModel> getAll(){
+		
+		Criteria criteria = getSession().createCriteria(GiverModel.class);
+		List<GiverModel> result = criteria.list();
+		
+		return result;
+	}
+	
+	//名字收尋 unfinish
+//	public GiverModel getByName(String name){
+//		Criteria criteria = getSession().createCriteria(GiverModel.class);
+//		criteria.add(Restrictions.eq("account", bean.getAccount()).ignoreCase());
+//		GiverModel model = (GiverModel) criteria.list().iterator().next();
+//
+//	}
+	
+
+	// 更新資料
+	public GiverModel update(GiverModel bean) {
+		Session session = sessionFactory.openSession();
+		
+		Criteria criteria = session.createCriteria(GiverModel.class)
+				.add(Restrictions.eq("account", bean.getAccount()).ignoreCase());
+		GiverModel model = (GiverModel) criteria.list().iterator().next();
+
+		bean.setId(model.getId());
+		session.close();
+		try {
+			getSession().update(bean);
+			return bean;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			System.out.println("update fail");
+		}
+		return null;
+
+	}
+
+	
+	
 }
--- src/test/java/tw/org/iiiedu/thegivers/dao/GiverDaoTest.java
+++ src/test/java/tw/org/iiiedu/thegivers/dao/GiverDaoTest.java
@@ -1,5 +1,9 @@
 package tw.org.iiiedu.thegivers.dao;
 
+import java.sql.Timestamp;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,33 +15,98 @@ public class GiverDaoTest extends GenericTest{
 	@Autowired
 	GiverDao dao;
 	
+//	@Test
+//	public void testGetByAccount() {
+//		Session session = dao.getSession();
+//		Transaction tx = session.beginTransaction();
+//		
+//		GiverModel bean = dao.getByAccount("aa");
+//		System.out.println(bean);
+//		tx.commit();
+//	}
+	
+//	@Test
+//	public void testInsert(){
+//		Session session = dao.getSession();
+//		Transaction tx = session.beginTransaction();
+//		
+//		GiverModel bean = new GiverModel();
+//		bean.setAccount("Jsona");
+//		bean.setAddress("台北市大安區");
+//		bean.setBirth(new Timestamp((new java.util.Date()).getTime()));
+//		bean.setEmail("Json@gmail.com");
+//		bean.setFamilyName("王");
+//		bean.setGender(true);
+//		bean.setGetInfo(true);
+//		bean.setHeadshot(null);
+//		bean.setIdNumber("A123456788");
+//		bean.setName("德馬");
+//		bean.setPasswd("sa123456");
+//		bean.setTel("0931468725");
+//		bean.setValid(true);
+//		
+//		dao.insert(bean);
+//		System.out.println(bean);
+//		tx.commit();
+//	}
+
 	@Test
-	public void testGetByAccount() {
-		GiverModel bean = dao.getByAccount("goodman9527");
-		System.out.println(bean);
+	public void testUpdate(){
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
 		
+		GiverModel bean = new GiverModel();
+		bean.setAccount("Jsona");
+		bean.setAddress("台北市大安區");
+		bean.setBirth(new Timestamp((new java.util.Date()).getTime()));
+		bean.setEmail("Jsona@gmail.com");
+		bean.setFamilyName("張");
+		bean.setGender(true);
+		bean.setGetInfo(true);
+		bean.setHeadshot(null);
+		bean.setIdNumber("A123456788");
+		bean.setName("君亞");
+		bean.setPasswd("sa123456");
+		bean.setTel("0931468725");
+		bean.setValid(true);
+		
+		bean = dao.update(bean);
+		System.out.println("test  "+bean);
+		tx.commit();
 	}
 	
-	@Test
-	public void testInsert(){
-		GiverModel bean = new GiverModel();
-//		bean.setAccount(account);
-//		bean.setAddress(address);
-//		bean.setBirth(birth);
-//		bean.setEmail(email);
-//		bean.setFamilyName(familyName);
-//		bean.setGender(gender);
-//		bean.setGetInfo(getInfo);
-//		bean.setHeadshot(headshot);
-//		bean.setId(id);
-//		bean.setIdNumber(idNumber);
-//		bean.setName(name);
-//		bean.setPasswd(passwd);
-//		bean.setTel(tel);
-//		bean.setValid(valid);
-		
-		
-		dao.insert(bean);
-	}
-
+//	@Test 
+//	public void testGetById(){
+//		Session session = dao.getSession();
+//		Transaction tx = session.beginTransaction();
+//		
+//		GiverModel bean = dao.getById(3);
+//		System.out.println(bean);
+//		tx.commit();
+//	}
+	
+//	@Test
+//	public void testHide(){
+//		Session session = dao.getSession();
+//		Transaction tx = session.beginTransaction();
+//		
+//		dao.hide("Json");
+//		tx.commit();
+//	}
+	
+//	@Test
+//	public void testGetAll(){
+//		Session session = dao.getSession();
+//		Transaction tx = session.beginTransaction();
+//		
+//		List<GiverModel> list = dao.getAll();
+//		Iterator iterator = list.iterator();
+//		while(iterator.hasNext()){
+//			System.out.println(iterator.next());
+//		}
+//		tx.commit();
+//	}
+	
+	
+	
 }
