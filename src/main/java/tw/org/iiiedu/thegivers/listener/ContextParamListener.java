dcc445b1d25b705e9411970c4fd7762f79329913
Rhys <godjdc@gmail.com>
2015-04-22 15:52:31 +0800

    log4j

--- src/main/java/tw/org/iiiedu/thegivers/listener/ContextParamListener.java
+++ src/main/java/tw/org/iiiedu/thegivers/listener/ContextParamListener.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -11,7 +12,8 @@ import tw.org.iiiedu.thegivers.service.CampaignService;
 import tw.org.iiiedu.thegivers.service.GiverService;
 
 public class ContextParamListener implements ServletContextListener{
-
+	
+	private static Logger log = Logger.getLogger(ContextParamListener.class);
 	@Autowired
 	private CampaignService campaignService;
 	@Autowired
@@ -25,6 +27,8 @@ public class ContextParamListener implements ServletContextListener{
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
+		log.debug("Running ContextParamListener");
+		
 		ServletContext sc = sce.getServletContext();
 
 		WebApplicationContextUtils.getRequiredWebApplicationContext(sc).getAutowireCapableBeanFactory().autowireBean(this);
