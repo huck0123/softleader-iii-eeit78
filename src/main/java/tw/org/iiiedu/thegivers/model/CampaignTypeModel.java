ea8a093b94ba615c8be0643f720bfc0ac7ac14d7
huck0123 <blithehuang0123@gmail.com>
2015-05-20 15:48:03 +0800

    多一個campaign_type table

---
+++ src/main/java/tw/org/iiiedu/thegivers/model/CampaignTypeModel.java
@@ -0,0 +1,25 @@
+package tw.org.iiiedu.thegivers.model;
+
+public class CampaignTypeModel {
+
+	private Integer id;
+	
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
